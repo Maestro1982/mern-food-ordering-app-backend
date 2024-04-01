a5b883d1e1a31c0accace846d8e38de44a212059
Maestro1982 <kris.tote@telenet.be>
2024-04-01 14:38:46 +0200

    modify package.json, index.ts in backend src folder, restaurant.ts in backend src models folder, create OrderController.ts in backend src controllers folder, OrderRoute.ts in backend src routes folder

---
+++ src/routes/OrderRoute.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+import { jwtCheck, jwtParse } from '../middleware/auth';
+
+import OrderController from '../controllers/OrderController';
+
+const router = express.Router();
+
+// /api/order => endpoints
+router.post(
+  '/checkout/create-checkout-session',
+  jwtCheck,
+  jwtParse,
+  OrderController.createCheckoutSession
+);
+
+export default router;
